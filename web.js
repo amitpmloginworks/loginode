d882204c4ed211aad103a630a10f5560d9fc2f36
Loginworks Softwares <38474202+amitpmloginworks@users.noreply.github.com>
2018-12-18 17:22:37 +0530

    sdsd
    sdsd

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var PORT = process.env.PORT || 5000
 var http = require('http');
 var client = new net.Socket();
 
-
+console.log(process.env.port)
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World changed!');
