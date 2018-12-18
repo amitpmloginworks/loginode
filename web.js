d8f1c4ae1e07766905cf28be693fc8ef509eaa72
Loginworks Softwares <38474202+amitpmloginworks@users.noreply.github.com>
2018-12-18 17:40:11 +0530

    Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 require('dotenv').config({silent: true});
 var net = require('net');
 var HOST = '10.0.0.183';   
+console.log('create process.env', process.env);
 var PORT = process.env.PORT || 5000
 var http = require('http');
 var client = new net.Socket();
 
 
 http.createServer(function (req, res) {
+  console.log('create server');
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('manager changed!');
  
