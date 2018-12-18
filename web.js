93c46c562d12561dc182690a4ea8b0c33fef231a
DESKTOP-ATCEUSN\abc <deepyatash786@gmail.com>
2018-12-18 17:16:48 +0530

    First commit

---
+++ web.js
@@ -0,0 +1,22 @@
+require('dotenv').config({silent: true});
+var net = require('net');
+var HOST = '10.0.0.183';   
+var PORT = process.env.PORT || 5000
+var http = require('http');
+var client = new net.Socket();
+
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World changed!');
+ 
+  client.connect(PORT, HOST, function() {
+  
+  console.log('CONNECTED TO: ' + HOST + ':' +PORT);
+  process.exit();
+  
+  
+  })
+
+
+}).listen(process.env.PORT || 3000); 
