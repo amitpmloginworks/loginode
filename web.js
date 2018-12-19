09f583d741440e0ee2ee923de08d21080a18db66
Loginworks Softwares <38474202+amitpmloginworks@users.noreply.github.com>
2018-12-19 17:21:37 +0530

    port changed

--- web.js
+++ web.js
@@ -2,7 +2,7 @@
 var net = require('net');
 var HOST = '10.0.0.67';   
 console.log('create process.env', process.env);
-var PORT = process.env.PORT || 80
+var PORT = process.env.PORT || 3000
 var http = require('http');
 var client = new net.Socket();
 
